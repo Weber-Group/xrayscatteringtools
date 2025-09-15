0d8b51828701aa846b099cbb10f35af1b34b0049
David Romano <150067936+davidjamesromano@users.noreply.github.com>
2025-09-15 17:17:47 -0400

    Added elastic and inelastic ab initio patterns for SF6
    Also added a test file for the patterns module.

--- src/xrayscatteringtools/theory/patterns.py
+++ src/xrayscatteringtools/theory/patterns.py
@@ -13,7 +13,9 @@ def __getattr__(name):
         with h5py.File(f"{data_path}/sf6.h5", "r") as f:
             q = f["q"][:]     # read dataset into memory
             I_q = f["I_q"][:]
-        obj = SimpleNamespace(q=q, I_q=I_q)
+            I_q_elastic = f["I_q_elastic"][:]
+            I_q_inelastic = f["I_q_inelastic"][:]
+        obj = SimpleNamespace(q=q, I_q=I_q, I_q_elastic=I_q_elastic, I_q_inelastic=I_q_inelastic)
         obj.__doc__ = """
         Ab initio SF6 Scattering Data.
         
@@ -23,10 +25,15 @@ def __getattr__(name):
             Momentum transfer values (q) in inverse angstroms.
         I_q : ndarray of shape (250,)
             Scattering intensity values corresponding to `q`.
+        I_q_elastic : ndarray of shape (250,)
+            Elastic scattering intensity values corresponding to `q`.
+        I_q_inelastic : ndarray of shape (250,)
+            Inelastic scattering intensity values corresponding to `q`.
 
         Notes
         -----
-        This data was received from Andres M. Carrascosa on Aug 26, 2025. Andres notes that this data was calculated at the CCSD/AVDZ level of theory.
+        The total scattering data was received from Andres M. Carrascosa on Aug 26, 2025. The elastic calculation was received on Sept 15, 2025.
+        Andres notes that both were calculated at the CCSD/AVDZ level of theory. The conversion from a.u. to 1/Angstrom was done using the factor 1.8897261246257702.
         """
         return obj
     raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
